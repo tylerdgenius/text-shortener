f48d04a7d038a8c8fb21e5acbff44957f61cf5aa
tylerdgenius <albertnwachukwu@gmail.com>
2022-07-10 13:36:11 +0100

    Added Code

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+/**
+ * This page defines the entire package
+ * @author tylerdgenius
+ */
+
+/**
+ * Define the base type for the function
+ */
+type textTruncatorType = (value: string, length?: number) => string;
+
+export const truncateText: textTruncatorType = (value, length) => {
+  if (length) {
+    /**
+     * If length is equal to the length of the string passed in then return the exact string
+     */
+    if (length === value.length) return value;
+
+    /**
+     * If it is not equal, then truncate the returned text and return it
+     */
+    return `${value.slice(0, length)}...`;
+  }
+
+  /**
+   * If length is equal to the length of the string passed in then return the exact string
+   */
+  if (value.length <= 30) return value;
+
+  /**
+   * If it is not equal, then truncate the returned text and return it
+   */
+  return `${value.slice(0, 30)}...`;
+};
