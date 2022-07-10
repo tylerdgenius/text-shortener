50e066e39396d1ef0f4961386ca7a9b4590e18fd
tylerdgenius <albertnwachukwu@gmail.com>
2022-07-10 14:39:32 +0100

    Patched Files

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,6 +1,31 @@
 import { truncateText } from '../index';
 
+/**
+ * Check if text is returned if characters are less than 30
+ */
 test('Truncate Text', () => {
   expect(truncateText('This')).toBe('This');
 });
 
+/**
+ * Check if text is truncated at 30 characters
+ */
+test('Truncate Text', () => {
+  expect(truncateText('This is supposed to be a text string that is used to give out an output')).toBe(
+    'This is supposed to be a text ...',
+  );
+});
+
+/**
+ * Check if text length is equal to character length passed in
+ */
+test('Truncate Text', () => {
+  expect(truncateText('This', 4)).toBe('This');
+});
+
+/**
+ * Check if text length is more than character length passed in
+ */
+test('Truncate Text', () => {
+  expect(truncateText('This', 2)).toBe('Th...');
+});
